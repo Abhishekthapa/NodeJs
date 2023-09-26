1ed5c854678759a2a48acfbf182c7274f21ec9e0
Abhishekthapa <acevalentyne5@gmail.com>
2023-09-26 13:24:16 +0545

    learnt built in npm package and how to use it

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const helpers = require("./helpers");
 //object destructing i.e. like importing a function from a class rather than whole class. Line 1 imports whole helpers.js but line 3  imports a method only.
 const {diff, mult} = require("./helpers");
+const http = require("http");
 
 
 console.log("Sum Total: " + helpers.sum(10,1));
 console.log("Difference Total: " + diff(10,1));
 console.log("Product Total: " + mult(10,5));
+
+const server = http.createServer((req,res)=>{
+    res.end("Hello World from node js modified");
+});
+server.listen(3000);
