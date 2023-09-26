963d48785077bdab0f4440ab160896c3f1b40140
Abhishekthapa <acevalentyne5@gmail.com>
2023-09-26 13:04:55 +0545

    object destructing and arrow function along with export modules

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const helpers = require("./helpers");
+//object destructing i.e. like importing a function from a class rather than whole class. Line 1 imports whole helpers.js but line 3  imports a method only.
+const {diff, mult} = require("./helpers");
 
 
-console.log("Process: ", process);
-console.log(helpers.sum(10,1));
+console.log("Sum Total: " + helpers.sum(10,1));
+console.log("Difference Total: " + diff(10,1));
+console.log("Product Total: " + mult(10,5));
--- helpers.js
+++ helpers.js
@@ -1,12 +1,20 @@
-function sum (a,b) {
-    return a+b;
-}
-
+// function sum (a,b) {
+//     return a+b;
+// }
+/*
+// arrow function of above sum function
+const sum = (a, b) => a + b;
 
 //for exporting sum function to exports{} of a process where every information of node is retained
 module.exports = {
     sum
 } 
+*/
+//above arrow function and module export can be written as : here the sum becomes anonymous arrow function with no name in exports of process
+exports.sum =  (a, b) => a + b;
+exports.diff =  (a, b) => a - b;
+exports.mult =  (a, b) => a * b;
+
 
 //this console log of process will only show above module exported i.e.,  exports: { sum: [Function: sum] },. Other js files wont because exports is a private to each module.
-console.log("Process: ", process);
+//console.log("Process: ", process);
